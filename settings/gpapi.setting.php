18de82b16296a23b33dda59e63d07fdf9f3a8554
Patrick Figel <pfigel@greenpeace.org>
2019-12-20 15:46:01 +0100

    Update settings definition and switch to array type
    This updates the settings file to the latest standard and switches
    the type for gpapi_gender_to_prefix_map back to "Array" since
    serialization handling seems buggy still.

--- settings/gpapi.setting.php
+++ settings/gpapi.setting.php
@@ -1,32 +1,29 @@
 <?php
-/*
- * Settings metadata file
- */
-return array (
-  'gpapi_membership_type_id' => array(
-    'group_name' => 'gpapi',
-    'group' => 'gpapi',
+
+return [
+  'gpapi_membership_type_id' => [
     'name' => 'gpapi_membership_type_id',
     'type' => 'Integer',
+    'html_type' => 'text',
     'default' => 1,
-    'add' => '4.6',
+    'add' => '1.6',
+    'title' => ts('GPAPI: Default membership type ID'),
     'is_domain' => 1,
     'is_contact' => 0,
-  ),
-  'gpapi_gender_to_prefix_map' => array(
-    'group_name' => 'gpapi',
-    'group' => 'gpapi',
+  ],
+  'gpapi_gender_to_prefix_map' => [
     'name' => 'gpapi_gender_to_prefix_map',
-    'type' => 'String',
-    'serialize' => CRM_Core_DAO::SERIALIZE_JSON,
+    'type' => 'Array',
+    'html_type' => 'text',
     'default' => [
       'Female' => 'Ms.',
       '1' => 'Ms.',
       'Male' => 'Mr.',
       '2' => 'Mr.',
     ],
-    'add' => '4.6',
+    'add' => '1.7',
+    'title' => ts('GPAPI: Gender to Prefix map'),
     'is_domain' => 1,
     'is_contact' => 0,
-  ),
-);
+  ],
+];
