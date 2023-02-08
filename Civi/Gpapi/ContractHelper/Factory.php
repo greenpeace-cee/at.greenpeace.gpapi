d0606d6457cda39ef288879d9ca19aeffe94ad63
Manuel Flandorfer <mflandor@greenpeace.org>
2023-02-08 17:17:16 +0100

    GP-19493 Simplify ContractHelper\Factory

--- Civi/Gpapi/ContractHelper/Factory.php
+++ Civi/Gpapi/ContractHelper/Factory.php
@@ -2,6 +2,7 @@
 
 namespace Civi\Gpapi\ContractHelper;
 
+use \Civi\Api4;
 use \CRM_Utils_Array;
 
 class Factory {
@@ -15,7 +16,11 @@ class Factory {
     $psp = CRM_Utils_Array::value('payment_service_provider', $params);
     $membership_id = CRM_Utils_Array::value('contract_id', $params);
 
-    if (empty($psp)) return new Sepa($membership_id);
+    if (empty($psp)) {
+      $psp = isset($membership_id)
+        ? self::getPaymentServiceProviderForContract((int) $membership_id)
+        : 'civicrm';
+    }
 
     switch ($psp) {
       case 'adyen':
@@ -32,111 +37,51 @@ class Factory {
     }
   }
 
-  /**
-   * @param $membershipId
-   *
-   * @return \Civi\Gpapi\ContractHelper\Adyen|\Civi\Gpapi\ContractHelper\Sepa
-   * @throws \CiviCRM_API3_Exception
-   * @throws \Civi\Gpapi\ContractHelper\Exception
-   */
-  public static function createWithMembershipId($membershipId) {
-    $recurring_contribution_field = \CRM_Contract_CustomData::getCustomFieldKey(
-      'membership_payment',
-      'membership_recurring_contribution'
-    );
-    $contract = civicrm_api3('Contract', 'getsingle', [
-      'id' => $membershipId,
-      'api.ContributionRecur.get' => [
-        'id' => '$value.' . $recurring_contribution_field
-      ],
-      'api.SepaMandate.get' => [
-        'entity_table' => 'civicrm_contribution_recur',
-        'entity_id' => '$value.' . $recurring_contribution_field,
-        'api.SepaCreditor.getsingle' => [
-          'id' => '$value.creditor_id',
-        ]
-      ],
-      'check_permissions' => 0,
-    ]);
-    if (empty($contract[$recurring_contribution_field]) || empty($contract['api.ContributionRecur.get']['values'][0]['id'])) {
-      throw new Exception('No payment method associated with contract', Exception::PAYMENT_METHOD_INVALID);
-    }
-    if (empty($contract['api.SepaMandate.get']['values'][0]['id'])) {
-      // currently, all supported payment instruments are backed by SepaMandates
-      // this may change in the future
-      throw new Exception('No mandate associated with contract', Exception::PAYMENT_METHOD_INVALID);
-    }
-    $creditor = $contract['api.SepaMandate.get']['values'][0]['api.SepaCreditor.getsingle'];
-    switch ($creditor['creditor_type']) {
-      case 'SEPA':
-        return new Sepa($membershipId);
+  private static function getPaymentServiceProviderForContract(int $membership_id) {
+    try {
+      $contract_payment_links = civicrm_api3('ContractPaymentLink', 'get', [
+        'contract_id' => $membership_id,
+        'is_active'   => TRUE,
+        'return'      => 'contribution_recur_id',
+        'sequential'  => TRUE,
+      ]);
 
-      case 'PSP':
-        // this is a PSP creditor. sepa_file_format_id contains the actual PSP
-        $psp_name = civicrm_api3('OptionValue', 'getvalue', [
-          'return'            => 'name',
-          'option_group_id'   => 'sepa_file_format',
-          'value'             => $creditor['sepa_file_format_id'],
-          'check_permissions' => 0,
-        ]);
-        switch ($psp_name) {
-          case 'adyen':
-            return new Adyen($membershipId);
+      $recur_contrib_id = $contract_payment_links['values'][0]['id'];
 
-          default:
-            throw new Exception('Unsupported payment service provider "' . $psp_name . '"', Exception::PAYMENT_SERVICE_PROVIDER_UNSUPPORTED);
-        }
+      // --- Check whether an associated SEPA mandate exists --- //
 
-      default:
-        throw new Exception('Unsupported payment instrument', Exception::PAYMENT_INSTRUMENT_UNSUPPORTED);
-    }
-  }
+      $sepa_mandate = Api4\SepaMandate::get()
+        ->selectRowCount()
+        ->addWhere('entity_id', '=', $recur_contrib_id)
+        ->setLimit(1)
+        ->execute();
 
-  /**
-   * @param $membershipId
-   * @param $paymentInstrumentName
-   * @param $paymentServiceProvider
-   *
-   * @return \Civi\Gpapi\ContractHelper\Adyen|\Civi\Gpapi\ContractHelper\Sepa
-   * @throws \Civi\Gpapi\ContractHelper\Exception
-   */
-  public static function createWithMembershipIdAndPspData($membershipId, $paymentServiceProvider) {
-    switch ($paymentServiceProvider) {
-      case 'adyen':
-        return new Adyen($membershipId);
+      if ($sepa_mandate->rowCount > 0) return 'civicrm';
+          
+      // --- Check whether the recurring contribution uses an Adyen payment processor --- //
 
-      case 'civicrm':
-        return new Sepa($membershipId);
+      $recurring_contribution = Api4\ContributionRecur::get()
+        ->selectRowCount()
+        ->addJoin(
+          'PaymentProcessor AS payment_processor',
+          'INNER',
+          ['payment_processor_id', '=', 'payment_processor.id']
+        )
+        ->addJoin(
+          'PaymentProcessorType AS payment_processor_type',
+          'INNER',
+          ['payment_processor.payment_processor_type_id', '=', 'payment_processor_type.id']
+        )
+        ->addWhere('id', '=', $recur_contrib_id)
+        ->addWhere('payment_processor_type.name', '=', 'Adyen')
+        ->setLimit(1)
+        ->execute();
 
-      default:
-        throw new Exception(
-          'Unsupported payment service provider "' . $paymentServiceProvider . '"',
-          Exception::PAYMENT_SERVICE_PROVIDER_UNSUPPORTED
-        );
-    }
-  }
+      if ($recurring_contribution->rowCount > 0) return 'adyen';
 
-  /**
-   * @param array $params
-   *
-   * @return \Civi\Gpapi\ContractHelper\Adyen|\Civi\Gpapi\ContractHelper\Sepa
-   */
-  public static function createWithoutExistingMembership($params) {
-    if (empty($params['payment_service_provider'])) return new Sepa();
+    } catch (Exception $e) {}
 
-    switch ($params['payment_service_provider']) {
-      case 'adyen':
-        return new Adyen();
-
-      case 'civicrm':
-        return new Sepa();
-
-      default:
-        throw new Exception(
-          'Unsupported payment service provider "' . $params['payment_service_provider'] . '"',
-          Exception::PAYMENT_SERVICE_PROVIDER_UNSUPPORTED
-        );
-    }
+    return NULL;
   }
 
 }
