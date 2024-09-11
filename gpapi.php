39720e6dbe9c8cd083d45d54f943e131dc3b0edf
Patrick Figel <pfigel@greenpeace.org>
2024-09-11 19:32:33 +0200

    Upgrade civix

--- gpapi.php
+++ gpapi.php
@@ -24,15 +24,6 @@ function gpapi_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function gpapi_civicrm_xmlMenu(&$files) {
-  _gpapi_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -42,96 +33,12 @@ function gpapi_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function gpapi_civicrm_postInstall() {
-  _gpapi_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function gpapi_civicrm_uninstall() {
-  _gpapi_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function gpapi_civicrm_enable() {
   _gpapi_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function gpapi_civicrm_disable() {
-  _gpapi_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function gpapi_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _gpapi_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function gpapi_civicrm_managed(&$entities) {
-  _gpapi_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function gpapi_civicrm_caseTypes(&$caseTypes) {
-  _gpapi_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function gpapi_civicrm_angularModules(&$angularModules) {
-  _gpapi_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function gpapi_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _gpapi_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 /**
@@ -170,4 +77,3 @@ function gpapi_civicrm_alterAPIPermissions($entity, $action, &$params, &$permiss
   $permissions['newsletter']['unsubscribe'] = ['access Newsletter API'];
   $permissions['newsletter']['getgroups'] = ['access Newsletter API'];
 }
-
